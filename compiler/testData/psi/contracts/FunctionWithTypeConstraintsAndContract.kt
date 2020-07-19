6c39bdbec37addedb946782c41370ceebbd8cae9
Arsen Nagdalian <arsen.nagdalian@jetbrains.com>
2020-07-19 04:06:15 +0300

    Move the contracts tests to a separate folder and add a test for property accessors' contracts

--- compiler/testData/psi/contracts/FunctionWithTypeConstraintsAndContract.kt
+++ compiler/testData/psi/contracts/FunctionWithTypeConstraintsAndContract.kt
@@ -1,11 +1,11 @@
 fun someFunctionWithTypeConstraints<T, E>(arg: E?, block: () -> T): String
+    where T : MyClass,
+          E : MyOtherClass
     contract [
         returns() implies (arg != null),
         callsInPlace(block, EXACTLY_ONCE),
         someComplexContract(arg, block)
     ]
-    where T : MyClass,
-          E : MyOtherClass
 {
     block()
     arg ?: throw NullArgumentException()
